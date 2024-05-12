5e1f5b1a1a03a05489fd2c3d4c4c2a97aeb184b8
Amir Hossein Shokoohi <amirshokoohi64@gmail.com>
2024-05-12 04:41:54 +0200

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,9 +1,9 @@
 <?php
-<<<<<<< HEAD
 
-=======
+
+
 /*
->>>>>>> deea3b07c71f6cd4b20f9d92e9ee7fb75287000c
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
@@ -56,7 +56,7 @@ class DashboardController extends Controller
 //    }
 
 
-<<<<<<< HEAD
+
 }
-=======
->>>>>>> deea3b07c71f6cd4b20f9d92e9ee7fb75287000c
+
+
