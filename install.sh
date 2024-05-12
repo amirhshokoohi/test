91407f4a1688394f8f35b8c2f6cb4f39253f91c9
Amir Hossein Shokoohi <amirshokoohi64@gmail.com>
2024-05-12 17:58:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -211,9 +211,9 @@ npm install
 
 npm run build
 
-apt install netdata -y
-
 sed -i "s/\$cpuUsage =.*/\$cpuUsage = Http::get('http:\/\/$ipv4:19999\/api\/v1\/data?chart=system.cpu&after=-1&format=json');/" app/Livewire/Dashboard.php
+
+apt install netdata -y
 
 sed -i 's/bind socket to IP = 127.0.0.1/bind socket to IP = 0.0.0.0/' /etc/netdata/netdata.conf
 
