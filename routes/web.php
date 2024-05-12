ea70c2eb53c8c4a7216489c6c02ffb2c96443624
Amir Hossein Shokoohi <amirshokoohi64@gmail.com>
2024-05-12 04:02:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,14 @@ Route::post('/', 'App\Http\Controllers\LoginController@authenticated')->name('lo
 
 
 /*Route::middleware(['auth'])->group(function () {
-<<<<<<< HEAD
     Route::get('/admin/dashboard', \App\Http\Livewire\Dashboard::class)->name('dashboard');;
     Route::get('/admin/get-cpudata', \App\Http\Livewire\Dashboard::class);
     Route::get('/get-ramdata', \App\Http\Livewire\Dashboard::class)->name('admin.updateCharts');
     Route::get('/get-diskdata', \App\Http\Livewire\Dashboard::class)->name('admin.updateCharts');
-=======
     Route::get('/admin/dashboard', \App\Livewire\Dashboard::class)->name('dashboard');;
     Route::get('/admin/get-cpudata', \App\Livewire\Dashboard::class);
     Route::get('/get-ramdata', \App\Livewire\Dashboard::class)->name('admin.updateCharts');
     Route::get('/get-diskdata', \App\Livewire\Dashboard::class)->name('admin.updateCharts');
->>>>>>> deea3b07c71f6cd4b20f9d92e9ee7fb75287000c
     Route::resource('users', UserController::class);
 
 
@@ -51,7 +48,6 @@ Route::post('/', 'App\Http\Controllers\LoginController@authenticated')->name('lo
 });*/
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
-<<<<<<< HEAD
     Route::get('/dashboard', \App\Http\Livewire\Dashboard::class)->name('dashboard');
     Route::get('/get-cpudata', [\App\Http\Livewire\Dashboard::class, 'getCpuDataRoute']);
     Route::get('/get-ramdata', [\App\Http\Livewire\Dashboard::class, 'getRamDataRoute']);
@@ -59,7 +55,6 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
 
 
     Route::resource('users', UserController::class);
-=======
     Route::get('/dashboard', \App\Livewire\Dashboard::class)->name('dashboard');
     Route::get('/get-cpudata', [\App\Livewire\Dashboard::class, 'getCpuDataRoute']);
     Route::get('/get-ramdata', [\App\Livewire\Dashboard::class, 'getRamDataRoute']);
@@ -67,7 +62,6 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
 
 
     Route::get('/users', User::class)->name('user');
->>>>>>> deea3b07c71f6cd4b20f9d92e9ee7fb75287000c
 
 
     Route::post('/logout', function (Request $request) {
