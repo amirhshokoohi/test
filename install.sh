63b1b6901dcc7a439de1c9eafd818d8316a3e083
Amir Hossein Shokoohi <amirshokoohi64@gmail.com>
2024-05-12 20:37:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -211,7 +211,7 @@ npm install
 
 npm run build
 
-sed -i "s/\(\$cpuUsage = \).*/\1Http::get('http:\/\/$ipv4:19999\/api\/v1\/data?chart=system.cpu&after=-1&format=json');/" app/Livewire/Dashboard.php
+sed -i "s/'http:\/\/\$serverIp:19999\/api\/v1\/data?chart=system.cpu&after=-1&format=json'/'http:\/\/$ipv4:19999\/api\/v1\/data?chart=system.cpu&after=-1&format=json'/g" app/Livewire/Dashboard.php
 
 apt install netdata -y
 
